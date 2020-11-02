3225888f396f2b7d5adba6ed0bfc7f1d4fcfac42
MJ <michaljalowik@gmail.com>
2020-11-02 11:57:12 +0100

    added Thymeleaf framework and devtools

---
+++ web/src/main/java/academy/learnprogramming/config/WebConfig.java
@@ -0,0 +1,18 @@
+package academy.learnprogramming.config;
+
+import academy.learnprogramming.util.ViewNames;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName(ViewNames.HOME);
+    }
+
+}
